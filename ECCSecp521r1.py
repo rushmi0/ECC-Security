037cdb662ed40f0f8236c50ddb246b22a15ac9dd
อุ้ อุ้ อ๊า อ๊า อุก่ะ อุก่ะ <120770468+rushmi0@users.noreply.github.com>
2023-04-29 19:19:10 +0700

    Update ECCSecp521r1.py

--- ECCSecp521r1.py
+++ ECCSecp521r1.py
@@ -1,4 +1,5 @@
 class Secp521r1:
+    
     def __init__(self):
         self.A = 6864797660130609714981900799081393217269435300143305409394463459185543183397656052122559640661454554977296311391480858037121987999716643812574028291115057151
         self.B = 659873501825845607906911397389668342420684974786564569494856176035728606701266678260266724488125155903932043814080613
@@ -16,7 +17,7 @@ class Secp521r1:
         return y
 
     def double(self, point):
-        slope = ((3 * point["x"] ** 2) * self.modinv((2 * point["y"]))) % self.P  # using modular inverse to perform "division"
+        slope = ((3 * point["x"] ** 2) * self.modinv((2 * point["y"]))) % self.P
         x = (slope ** 2 - (2 * point["x"])) % self.P
         y = (slope * (point["x"] - x) - point["y"]) % self.P
         return {"x": x, "y": y}
@@ -34,8 +35,21 @@ class Secp521r1:
             point = self.G
         current = point
         binary = bin(k)[2:]
-        for Fbin_prv in binary[1:]:
+        for bin_prv in binary[1:]:
             current = self.double(current)
-            if Fbin_prv == "1":
+            if bin_prv == "1":
                 current = self.add(current, point)
         return current
+
+
+""" ตัวอย่างการใช้งาน """"
+curve = Secp521r1()
+private_key = "e38b1a0e3169055bbec35266002ee43519a545dd30c6315942ce82da60b5853d"
+k = int(private_key, 16)
+public_key = curve.multiply(k)
+
+x = hex(public_key["x"])[2:]
+y = hex(public_key["y"])[2:]
+
+print(f"X coordinate: {x}")
+print(f"Y coordinate: {y}")
