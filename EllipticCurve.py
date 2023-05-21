021efa469985a23482fe7c24d0a8cc2bbb119331
อุ้ อุ้ อ๊า อ๊า อุก่ะ อุก่ะ <120770468+rushmi0@users.noreply.github.com>
2023-05-21 18:12:37 +0700

    Update EllipticCurve.py

--- EllipticCurve.py
+++ EllipticCurve.py
@@ -1,51 +1,44 @@
 from random import getrandbits
 
-class EllipticCurve:
-
-    # Secp256k1
+class ECSecp256k1:
     def __init__(self):
         self.A = 0
         self.B = 7
-        # 2 ** 256 - 2 ** 32 - 2 ** 9 - 2 ** 8 - 2 ** 7 - 2 ** 6 - 2 ** 4 - 1
         self.P = 115792089237316195423570985008687907853269984665640564039457584007908834671663
         self.N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
-        self.G = {"x": 55066263022277343669578718895168534326250603453777594175500187360389116729240,
-                  "y": 32670510020758816978083085130507043184471273380659243275938904335757337482424}
+        self.G = (55066263022277343669578718895168534326250603453777594175500187360389116729240,
+                  32670510020758816978083085130507043184471273380659243275938904335757337482424)
 
-    # การคำนวณหลัก: Elliptic Curve cryptography
-    def modinv(self, A: int, N=None) -> int:
+
+    """ ------------------- การคำนวณหลัก Elliptic Curve cryptography ------------------- """
+    def modinv(self, A, N=None):
         if N is None:
             N = self.P
-            
-        def extended_gcd(a, b):
-            if b == 0:
-                return a, 1, 0
-            gcd, x, y = extended_gcd(b, a % b)
-            return gcd, y, x - (a // b) * y
+        return pow(A, -1, N)
 
-        gcd, x, _ = extended_gcd(A, N)
-        result = x % N
-        return result if result >= 0 else result + N
-
-    def double(self, point: int) -> dict:
-        lam_numer = (3 * point["x"] ** 2 + self.A) % self.P
-        lam_denom = (2 * point["y"]) % self.P
+    def double(self, point):
+        x, y = point
+        lam_numer = (3 * x * x + self.A) % self.P
+        lam_denom = (2 * y) % self.P
         lam = (lam_numer * self.modinv(lam_denom)) % self.P
 
-        xR = (lam ** 2 - 2 * point["x"]) % self.P
-        yR = (lam * (point["x"] - xR) - point["y"]) % self.P
-        return {"x": xR, "y": yR}
+        xR = (lam * lam - 2 * x) % self.P
+        yR = (lam * (x - xR) - y) % self.P
+        return (xR, yR)
 
-    def add(self, point1: int, point2: int) -> dict:
+    def add(self, point1, point2):
         if point1 == point2:
             return self.double(point1)
-        m = ((point2["y"] - point1["y"]) * self.modinv(point2["x"] - point1["x"])) % self.P
-        xR = (m ** 2 - point1["x"] - point2["x"]) % self.P
-        yR = (m * (point1["x"] - xR) - point1["y"]) % self.P
-        return {"x": xR, "y": yR}
+        x1, y1 = point1
+        x2, y2 = point2
 
-    def multiply(self, k: int, point=None) -> dict:
-        if not point:
+        m = ((y2 - y1) * self.modinv(x2 - x1)) % self.P
+        xR = (m * m - x1 - x2) % self.P
+        yR = (m * (x1 - xR) - y1) % self.P
+        return (xR, yR)
+
+    def multiply(self, k, point=None):
+        if point is None:
             point = self.G
         binary = bin(k)[2:]
         current = point
@@ -55,51 +48,39 @@ class EllipticCurve:
                 current = self.add(current, point)
         return current
 
+    """ ------------------- ส่วนขยาย ------------------- """
     def publickey(self, k: int) -> str:
         point = self.multiply(k)
-        return f"04{hex(point['x'])[2:]}{hex(point['y'])[2:]}"
+        return f"04{point[0]:x}{point[1]:x}"
 
-    def compress_point(self, public_key: str) -> str:
-        if public_key[0:2] != '04':
+    def compress_point(self, publickey_Hex: str) -> str:
+        if publickey_Hex[0:2] != '04':
             raise ValueError('Invalid Public Key')
-        x = int(public_key[2:66], 16)
-        y = int(public_key[66:], 16)
+        x = int(publickey_Hex[2:66], 16)
+        y = int(publickey_Hex[66:], 16)
 
-        if (y % 2) == 0:
-            public_key = '02' + format(x, '064x')
+        if y & 1 == 0:
+            publickey_Hex = '02' + format(x, '064x')
         else:
-            public_key = '03' + format(x, '064x')
-        return public_key
+            publickey_Hex = '03' + format(x, '064x')
+        return publickey_Hex
 
-    # การคำนวณหลัก: ECDSA
-    def signature(self, private_key: int, message: int) -> dict:
+    """ ------------------- การคำนวณหลัก สร้างลายเซ็นและตรวจสอบ ECDSA ------------------- """
+    def signing(self, private_key, message):
         z = message
-        k = 47015016470583645446654890691026687320549785451357444754315920288772675949303
-        #k = getrandbits(256)
+        #k = 42854675228720239947134362876390869888553449708741430898694136287991817016610
+        k = getrandbits(256)
         r, s = 0, 0
         while r == 0 or s == 0:
             point = self.multiply(k)
             k_inv = self.modinv(k, self.N)
-            r = point['x'] % self.N
+            r = point[0] % self.N
             s = ((z + r * private_key) * k_inv) % self.N
-        return {'r': r, 's': s}
+        return (r, s)
 
-    def to_der_format(self, signature: dict) -> str:
-        r = signature['r']
-        s = signature['s']
-
-        rb = r.to_bytes((r.bit_length() + 7) // 8, byteorder='big', signed=False)
-        sb = s.to_bytes((s.bit_length() + 7) // 8, byteorder='big', signed=False)
-
-        der_r = b'\x02' + len(rb).to_bytes(1, byteorder='big', signed=False) + rb
-        der_s = b'\x02' + len(sb).to_bytes(1, byteorder='big', signed=False) + sb
-        der_sig = b'\x30' + (len(der_r) + len(der_s)).to_bytes(1, byteorder='big', signed=False) + der_r + der_s
-        return der_sig.hex()
-
-    def verify(self, publickey: dict, message: int, signature: dict) -> bool:
+    def verify(self, publickey, message, Signature):
         m = message
-        r = signature['r']
-        s = signature['s']
+        r, s = Signature
 
         w = self.modinv(s, self.N)
         u1 = (m * w) % self.N
@@ -109,31 +90,45 @@ class EllipticCurve:
         point2 = self.multiply(u2, publickey)
 
         point = self.add(point1, point2)
-        x = point['x'] % self.N
+        x = point[0] % self.N
         return x == r
 
+    def to_der_format(self, Signature):
+        r, s = Signature
+        rb = r.to_bytes((r.bit_length() + 7) // 8, byteorder='big', signed=False)
+        sb = s.to_bytes((s.bit_length() + 7) // 8, byteorder='big', signed=False)
 
-curve = EllipticCurve()
-# private_key = getrandbits(256)
+        der_r = b'\x02' + len(rb).to_bytes(1, byteorder='big', signed=False) + rb
+        der_s = b'\x02' + len(sb).to_bytes(1, byteorder='big', signed=False) + sb
+        der_sig = b'\x30' + (len(der_r) + len(der_s)).to_bytes(1, byteorder='big', signed=False) + der_r + der_s
+        return der_sig.hex()
 
-private_key = 111798668807017442629247557499629816624858299873427551140682199544191852692645
-# print(hex(private_key)[2:])
-print("Private key:", private_key)
-message = 67190017246757783140308448604179518505030850719375738244213419124624541387587
 
-# สร้าง Public key
-P = curve.multiply(private_key)
-print("Key Point", P)
+if __name__ == "__main__":
+    curve = ECSecp256k1()
+    private_key = getrandbits(256)
+    #private_key = 111798668807017442629247557499629816624858299873427551140682199544191852692645
+    print("[H] Private key:", hex(private_key)[2:])
+    print("Private key:", private_key)
+    message = 67190017246757783140308448604179518505030850719375738244213419124624541387587
 
-public_key = curve.publickey(private_key)
-print("[U] Public Key:", public_key)
+    P = curve.multiply(private_key)
+    print("\nKey Point", P)
 
-compress = curve.compress_point(public_key)
-print("[C] Public Key:", compress)
+    publickey_Hex = curve.publickey(private_key)
+    print("[U] Public Key:", publickey_Hex)
 
-sign = curve.signature(private_key, message)
-der = curve.to_der_format(sign)
-print("Der format:", der)
+    compress = curve.compress_point(publickey_Hex)
+    print("[C] Public Key:", compress)
 
-isValid = curve.verify(P, message, sign)
-print("Signature is:", isValid)
+    sign = curve.signing(private_key, message)
+    print("\nSignature", sign)
+
+    der = curve.to_der_format(sign)
+    print("Der format:", der)
+
+    isValid = curve.verify(P, message, sign)
+    if isValid:
+        print("Signature is valid")
+    else:
+        print("Signature is invalid")
